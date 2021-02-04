a78347bcf98e87128cf4d551fea95eb9c6eb4faf
rbtkay <robert.g.khayat@gmail.com>
2021-02-04 17:06:34 +0100

    basic graph for correlations

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
 import streamlit as st
+import pymongo
 from pymongo import MongoClient
 import matplotlib as plot
 import pandas as pd
 import numpy as np
-
-
-# def load_mongo_data(collection):
-#     data = collection.find()
-#     return data.count()
-
 
 st.text('Spotify Dashboard')
 
@@ -24,6 +19,24 @@ spotify_db = client.spotify
 correlations = pd.DataFrame(list(spotify_db.correlations.find()), columns=[
                             "base_col", "compared_col", "value"])
 
+songs_per_duration = pd.DataFrame({
+    "time": ["0min", "1min", "3min", "6min", "9min", "12min", "15min"],
+    "count": [
+        spotify_db.songs.find(
+            {"duration_ms": {"$lt": 60000}}).count(),
+        spotify_db.songs.find(
+            {"duration_ms": {"$gte": 60000, "$lt": 180000}}).count(),
+        spotify_db.songs.find(
+            {"duration_ms": {"$gte": 180000, "$lt": 360000}}).count(),
+        spotify_db.songs.find(
+            {"duration_ms": {"$gte": 360000, "$lt": 540000}}).count(),
+        spotify_db.songs.find(
+            {"duration_ms": {"$gte": 540000, "$lt": 720000}}).count(),
+        spotify_db.songs.find(
+            {"duration_ms": {"$gte": 720000, "$lt": 900000}}).count(),
+        spotify_db.songs.find({"duration_ms": {"$gte": 900000}}).count()
+    ]
+}).set_index('time')
 
 df_valences = pd.DataFrame(
     correlations[correlations["base_col"] == "valence"], columns=["compared_col"])
@@ -36,10 +49,12 @@ correlations_danceability = pd.DataFrame(
     correlations[correlations["base_col"] == "danceability"], columns=["value"]).set_index(df_danceability['compared_col'])
 
 
-
-st.write("Correlation avec la valence")
+st.write("Correlation valence")
 st.bar_chart(correlations_valence)
 
-st.write("Correlation avec la danceability")
+st.write("Correlation danceability")
 st.bar_chart(correlations_danceability)
 
+# st.write(songs_per_duration)
+
+st.line_chart(songs_per_duration)
