c09a7b805e2b6e84232da27b4ed4372ddfc03cb3
rbtkay <robert.g.khayat@gmail.com>
2021-02-05 13:17:32 +0100

    [UPDATE] insert in hdfs on docker-compose up

--- work/insert_data.py
+++ work/insert_data.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 from pyspark.sql import SparkSession
 import sys
-import calendar
-import time
 spark = SparkSession.builder.getOrCreate()
 
-df = spark.read.format('csv').options(header=True, inferSchema=True).load(
-    '/home/jovyan/work/data/Spotify/genres_v2.csv')
-
-df.write.csv(f"hdfs://hadoop-master:9000/spotify_data_{calendar.timegm(time.gmtime())}")
-
+df = spark.read.format('csv').options(header=True, inferSchema=True).load('/home/jovyan/work/data/Spotify/genres_v2.csv')
+df.write.csv("hdfs://hadoop-master:9000/spotify_final_data")
 print("data inserted successfully")
